f8ac241e35a0ff470bd1dbed0f3a68b929221aeb
tina <px732amgo81sp8@gmail.com>
2020-12-04 23:18:25 +0800

    react-redux-blog

---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+import postReducer from './reducers/postReducer';
+import userReducer from './reducers/userReducer';
+
+
+export default configureStore({
+  reducer: {
+    posts: postReducer,
+    user: userReducer
+  },
+});
---
+++ src/utils.js
@@ -0,0 +1,15 @@
+const TOKEN_NAME = 'token'
+
+export const setAuthToken = token => {
+  localStorage.setItem(TOKEN_NAME,token)
+}
+export const getAuthToken = () => {
+  return localStorage.getItem(TOKEN_NAME)
+}
+export const createPaginate = (totalPages) => {
+  const pages = [];
+  for (let i = 1; i <= totalPages; i++) {
+    pages.push(i);
+  }
+  return pages;
+};
